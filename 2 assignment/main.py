4d5a19d9652aaf7b85494e2a554e5740c6200c0f
Mik1810 <michael.piccirilli@student.univaq.it>
2023-10-26 18:08:10 +0200

    Refactored the network code, now is modular and accept the same argument as plot does

--- 2 assignment/main.py
+++ 2 assignment/main.py
@@ -1,11 +1,16 @@
-import time
-
 from plot import Plot_env
+from network import Graph
 
+size = (8, 8)
+start = (4, 3)
+goal = (3, 6)
+walls = [(3, 3), (7, 3), (2, 4), (3, 5), (4, 5), (5, 5), (6, 5), (7, 5)]
 
 if __name__ == "__main__":
-    env = Plot_env()
+    env = Plot_env(size=size, start=start, goal=goal, walls=walls)
     env.draw_grid()
 
-    env.draw_cell(3, 3, "red")
-    env.draw_cell(5, 7, "blue")
+    graph = Graph(size=size, start=start, goal=goal, walls=walls)
+
+    #env.draw_cell(3, 3, "red")
+    #env.draw_cell(5, 7, "blue")
--- 2 assignment/plot.py
+++ 2 assignment/plot.py
@@ -3,37 +3,62 @@ import matplotlib.pyplot as plt
 
 class Plot_env:
 
-    def __init__(self, start=(0, 0), goal=(7, 7)):
+    def __init__(self, size=(8, 8), start=(0, 0), goal=(8, 8), walls=None):
+        if walls is None:
+            walls = []
+        self.x_size, self.y_size = size
         self.start = start
         self.goal = goal
+        self.walls = walls
         self.ax, self.fig = None, None
         plt.ion()
+
 
     def draw_grid(self):
 
         self.fig, self.ax = plt.subplots()
 
         # Make an 8x8 grid
-        for i in range(8):
-            for j in range(8):
-                self.ax.add_patch(plt.Rectangle((i, j), 1, 1, facecolor="white", fill=True, edgecolor="black"))
+        for x in range(self.x_size):
+            for y in range(self.y_size):
+                self.ax.add_patch(plt.Rectangle((x, y), 1, 1, facecolor="white", fill=True, edgecolor="black"))
 
         for i in range(1, 9):
             self.ax.text(i - 0.5, -0.5, str(i), fontsize=12, ha='center', va='center')
             self.ax.text(-0.5, i - 0.5, str(i), fontsize=12, ha='center', va='center')
 
-        # Set limits for the axes
+        #Draw walls
+        for couple in self.walls:
+            x, y = couple
+            self.ax.add_patch(plt.Rectangle((x - 1, y - 1), 1, 1, facecolor="black", fill=True, edgecolor="black"))
+
+        #Draw start and goal
+        start_x, start_y = self.start
+        goal_x, goal_y = self.goal
+        start_rect = plt.Rectangle((start_x - 1, start_y - 1), 1, 1, facecolor="green", fill=True, edgecolor="black")
+        self.ax.add_patch(start_rect)
+        self.ax.add_patch(plt.Rectangle((goal_x - 1, goal_y - 1), 1, 1, facecolor="blue", fill=True, edgecolor="black"))
+
+        #Add labels to start and goal cells
+        label_s_x = start_x + start_rect.get_width() / 2.0
+        label_s_y = start_y + start_rect.get_height() / 2.0
+        label_g_x = goal_x + start_rect.get_width() / 2.0
+        label_g_y = goal_y + start_rect.get_height() / 2.0
+        self.ax.annotate("s", (label_s_x - 1, label_s_y - 1), color='black', weight='bold', fontsize=10, ha='center', va='center')
+        self.ax.annotate("g", (label_g_x - 1, label_g_y - 1), color='black', weight='bold', fontsize=10, ha='center', va='center')
+
+        #Set limits for the axes
         self.ax.set_xlim(0, 8)
         self.ax.set_ylim(0, 8)
 
         plt.axis('equal')
         plt.axis('off')
-        plt.show()
-        plt.pause(2)
+        #plt.show()
+        #plt.pause(20)
 
     def draw_cell(self, x, y, color):
         # Add a colored cell
         self.ax.add_patch(plt.Rectangle((x - 1, y - 1), 1, 1, facecolor=color, fill=True, edgecolor="black"))
         self.fig.canvas.draw()
-        plt.show()
-        plt.pause(2)
+        #plt.show()
+        #plt.pause(2)
